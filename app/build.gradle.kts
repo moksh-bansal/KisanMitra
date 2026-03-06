cee8b44cc95013da0cb1ec9a1f07f9bd16f5be18
moksh-bansal <mokshbansal930@gmail.com>
2026-03-06 15:39:20 +0530

    Home page, Disease page, Market page UI changes for mobile.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.appdev.kisanmitra"
-        minSdk = 26
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
